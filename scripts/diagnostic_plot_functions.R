7ea7e189915baa04879fb80c686db7c07e7996e5
Jon Falciani <83192941+falcianij@users.noreply.github.com>
2026-02-24 13:12:26 -0800

    Retool plotting to raw segmented rasters and cleaner legends

--- scripts/diagnostic_plot_functions.R
+++ scripts/diagnostic_plot_functions.R
@@ -6,14 +6,51 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(scales)
+
+legend_bar_guide <- function() {
+  guide_colorbar(
+    direction = "horizontal",
+    title.position = "top",
+    label.position = "bottom",
+    frame.colour = "grey50",
+    ticks = TRUE,
+    ticks.colour = "grey50",
+    frame.linewidth = 0.5,
+    barwidth = grid::unit(4.0, "cm"),
+    barheight = grid::unit(0.35, "cm")
+  )
+}
 
 plot_available_energy <- function(df, x = c("B_used", "f_ref", "T"), y = "pO2") {
   x <- match.arg(x)
-  ggplot(df, aes(x = .data[[x]], y = .data[[y]], fill = E_net_norm)) +
-    geom_raster(interpolate = TRUE) +
-    scale_fill_gradient2(low = "#7f0000", mid = "white", high = "#08519c", midpoint = 0,
-                         name = "E_avail / Cmax", na.value = "grey20") +
-    labs(x = x, y = y, title = "Available energy diagnostic")
+
+  dd <- df %>% mutate(.z = E_net_norm)
+  dd_na <- dd %>% filter(!is.finite(.z) | is.na(.z))
+  dd_neg <- dd %>% filter(is.finite(.z) & .z < 0)
+  dd_pos <- dd %>% filter(is.finite(.z) & .z >= 0) %>% mutate(.zpos = .z)
+
+  ggplot() +
+    geom_raster(data = dd_na, aes(.data[[x]], .data[[y]]), fill = "black", interpolate = TRUE) +
+    geom_raster(data = dd_neg, aes(.data[[x]], .data[[y]]), fill = "gray60", interpolate = TRUE) +
+    geom_raster(data = dd_pos, aes(.data[[x]], .data[[y]], fill = pmin(.zpos, 1)), interpolate = TRUE) +
+    scale_fill_gradient(
+      low = "white", high = "#67A5CA", na.value = "black",
+      limits = c(0, 1), breaks = seq(0, 1, by = 0.2), labels = label_number(accuracy = 0.1),
+      oob = squish,
+      name = expression("Available energy"),
+      guide = legend_bar_guide()
+    ) +
+    geom_contour(
+      inherit.aes = FALSE,
+      data = dd_pos, aes(x = .data[[x]], y = .data[[y]], z = .zpos),
+      breaks = seq(0, 1, by = 0.1), colour = "black", linewidth = 0.3, alpha = 1, linetype = "dashed"
+    ) +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    labs(x = x, y = y, title = "Available energy diagnostic") +
+    theme_minimal(base_size = 9) +
+    theme(aspect.ratio = 1, legend.position = "bottom", legend.box = "horizontal")
 }
 
 plot_metabolic_allocation <- function(df, x = c("B_used", "f_ref", "T"), y = "pO2") {
@@ -29,7 +66,9 @@ plot_metabolic_allocation <- function(df, x = c("B_used", "f_ref", "T"), y = "pO
   ggplot(dd, aes(x = xvar, y = yvar, fill = value)) +
     geom_raster(interpolate = TRUE) +
     facet_wrap(~process, scales = "free") +
-    scale_fill_viridis_c(option = "C", na.value = "grey20") +
+    scale_fill_viridis_c(option = "C", na.value = "black", guide = legend_bar_guide()) +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
     labs(x = x, y = y, title = "Metabolic allocation diagnostics")
 }
 
@@ -52,6 +91,8 @@ plot_process_reference <- function(df, x = c("B_used", "f_ref"), y = "pO2") {
   ggplot(dd, aes(x = xvar, y = yvar, fill = value)) +
     geom_raster(interpolate = TRUE) +
     facet_wrap(~diagnostic, scales = "free") +
-    scale_fill_viridis_c(option = "D", na.value = "grey20") +
+    scale_fill_viridis_c(option = "D", na.value = "black", guide = legend_bar_guide()) +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
     labs(x = x, y = y, title = "Process-level reference diagnostics")
 }
